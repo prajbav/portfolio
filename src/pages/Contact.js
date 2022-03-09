ab682ea522521eda7b173aea6f9a1f81e251106c
Prajakta Bavikar <prajakta.bavikar@gmail.com>
2022-03-09 00:46:16 -0500

    mcontact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,46 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNav } from "../customHooks/useNav";
 import "./Page.css";
 import "../styles/contact.css";
+import { send } from "emailjs-com";
+import { init } from "@emailjs/browser";
+
 const Response = () => {
   return <p>I will get back to you.</p>;
 };
 
 const Contact = () => {
-  // useNav takes in a navLinkId and returns a ref
-  // this ref is used to register the navLinkId that's
-  // currently in view, and apply activeClass styling
-  // to the corresponding nav childElement
+  const [toSend, setToSend] = useState({
+    from_name: "prajkta.bavikar@gmail.com",
+    to_name: "prajakta.bavikar@gmail.com",
+    message: "hey you are amazing",
+    reply_to: "prajakta.bavikar@gmail.com",
+  });
 
   const contactRef = useNav("Contact");
+  init("user_Xr5EUDkhltEXrfXF4I0tp");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    send(
+      "service_portfolio",
+      "template_portfolio",
+      toSend,
+      "user_Xr5EUDkhltEXrfXF4I0tp"
+    )
+      .then((response) => {
+        console.log("SUCCESS!", response.status, response.text);
+      })
+      .catch((err) => {
+        console.log("FAILED...", err);
+      });
+  };
 
   return (
     <section ref={contactRef} id="contactContainer">
       {/* <div className="contact-background"> */}
       <div className="contact-form">
-        <form action="">
+        <form>
+          <h2>Lets Connect!</h2>
           <div className="form-group">
-            <h2>Lets Connect!</h2>
-            <span>Full Name</span>
-            <br />
-            <input
-              className="input-fullname"
-              type="text"
-              required
-              name="fullname"
-            />
-            <br />
-            <span>Email</span>
-            <br />
-            <input className="input-email" type="text" required name="email" />
-            <br />
-          </div>
-          <div className="form-group">
-            <span>Message</span>
-            <br />
-            <textarea className="" required name="message" />
-            <br />
-            <button type="submit" />
+            <div className="fullname">
+              <input
+                className="input-firstname"
+                type="text"
+                required
+                name="firstname"
+              />
+              <input
+                className="input-lastname"
+                type="text"
+                required
+                name="lastname"
+              />
+            </div>
+            <input className="input-email" type="email" required name="email" />
+            {/* </div> */}
+            {/* <div className="form-group"> */}
+            <textarea className="text-content" required name="message" />
+
+            <button type="submit" className="btn-submit" onSubmit={onSubmit}>
+              Send
+            </button>
             {/* <div className="response">{response ? <Response /> : null} </div> */}
           </div>
         </form>
